6a65d70e1aaf02230d8350bdaf47bd3db21dc82e
Bishal Kharel <bishalkharel9842@gmail.com>
2025-05-24 13:00:19 +0545

    Extracting Product Price and changed URl

--- html_scraper/amazon_scraper.py
+++ html_scraper/amazon_scraper.py
@@ -13,11 +13,32 @@ def get_page_html(url):
     res= requests.get(url=url, headers=REQUEST_HEADER)
     return res.content
 
+def get_product_price(soup):
+    main_price_span = soup.find('span', attrs={
+        'class': 'a-price-whole'
+    })
+
+    if main_price_span:
+        price = main_price_span.text.strip().replace(",", "")
+        # print(f"💰 Price found: {price}")
+        return price
+    else:
+        print("⚠️ Price not found.")
+        return "N/A"
+
+    # price_span = main_price_span.findAll('span')
+    # for span in price_span:
+    #     price = span.text.strip().replace("$","").replace(",","")
+    #     print(price)
+
+
 def extract_product_info(url):
     product_info = {}
     print (f'Scarping URl: {url}')
     html = get_page_html(url=url)
-    print(html)
+    soup = bs4.BeautifulSoup(html, 'lxml')
+    product_info['price'] = get_product_price(soup)
+    return product_info
 
 if __name__ =="__main__":
 
