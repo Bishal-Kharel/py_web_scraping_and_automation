3082dd8e07f6ebe1dbf3c6f9ba5afee78a66f080
Bishal Kharel <bishalkharel9842@gmail.com>
2025-05-25 20:14:37 +0545

    Added more urls and completed html_scraping function

--- html_scraper/amazon_scraper.py
+++ html_scraper/amazon_scraper.py
@@ -1,13 +1,18 @@
 from datetime import datetime
+from itertools import product
 import requests
 import csv
 import bs4
+import concurrent.futures
+from tqdm import tqdm
 
 USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
 REQUEST_HEADER = {
     'User-Agent' : USER_AGENT,
     "Accept-Language": "en-US,en;q=0.9",
 }
+
+NO_THREADS = 10
 
 def get_page_html(url):
     res= requests.get(url=url, headers=REQUEST_HEADER)
@@ -20,30 +25,64 @@ def get_product_price(soup):
 
     if main_price_span:
         price = main_price_span.text.strip().replace(",", "")
-        # print(f"💰 Price found: {price}")
         return price
     else:
         print("⚠️ Price not found.")
         return "N/A"
 
-    # price_span = main_price_span.findAll('span')
-    # for span in price_span:
-    #     price = span.text.strip().replace("$","").replace(",","")
-    #     print(price)
+def get_product_title(soup):
+        product_title = soup.find('span', id = 'productTitle')
+        return product_title.text.strip()
 
+def get_product_rating(soup):
+    product_ratings_div = soup.find('div', attrs={
+        'id': 'averageCustomerReviews'
+    })
+    product_rating_section = product_ratings_div.find('i', class_='a-icon-star')
+    product_rating_span = product_rating_section.find('span')
+    try:
+        rating = product_rating_span.text.strip().split()
+        return float(rating[0])
+    except ValueError:
+        print("Value Obtainned for Rating could not parsed")
+
+def get_product_technical_details(soup):
+    details ={}
+    technical_details_section = soup.find('div', id='prodDetails')
+    data_table = technical_details_section.findAll('table', id="productDetails_techSpec_section_1")
+
+    for table in data_table:
+        table_rows = table.findAll('tr')
+        for row in table_rows:
+            row_key =row.find('th').text.strip()
+            row_value = row.find('td').text.strip().replace('\u200e', '')
+            details[row_key]= row_value
+    return details
 
 def extract_product_info(url):
     product_info = {}
-    print (f'Scarping URl: {url}')
+    print (f'Scraping URl: {url}')
     html = get_page_html(url=url)
     soup = bs4.BeautifulSoup(html, 'lxml')
+    product_info['title'] = get_product_title(soup)
     product_info['price'] = get_product_price(soup)
+    product_info['rating'] = get_product_rating(soup)
+    product_info.update(get_product_technical_details(soup))
     return product_info
 
 if __name__ =="__main__":
-
+    products_data =[]
+    # urls =[]
     with open("amazon_products_urls.csv", newline="") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
+        reader = list(csv.reader(csvfile, delimiter=","))
         for row in reader:
             url = row[0]
-            print(extract_product_info(url))
+            products_data.append(extract_product_info(url))
+
+
+    output_file_name ='output-{}.csv'.format(datetime.today().strftime("%m-%d-%Y"))
+    with open(output_file_name,'w') as outputFile:
+        writer = csv.writer(outputFile)
+        writer.writerow(products_data[0].keys())
+        for products in products_data:
+            writer.writerow(products.values())
